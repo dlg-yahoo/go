8af6dbea09754e94a8335e558c054f899b9235aa
David Leon Gil <dgil@yahoo-inc.com>
2014-12-29 18:33:36 -0800

    crypto/ecdsa: make nonce-reuse safe even with broken entropy source
    crypto/ecdsa/ecdsa_test.go:
      TestNonceSafety: Check that signatures are safe even with a broken entropy source.
      TestINDCCA: Check that signatures remain non-deterministic with a functional entropy source.

---
+++ src/crypto/ecdsa/util.go
@@ -0,0 +1,19 @@
+package ecdsa
+
+import (
+	"io"
+)
+
+type zr struct {
+	io.Reader
+}
+
+// Read replaces the contents of dst with zeros.
+func (z *zr) Read(dst []byte) (n int, err error) {
+	for i := range dst {
+		dst[i] = 0
+	}
+	return len(dst), nil
+}
+
+var zeroReader = &zr{}
